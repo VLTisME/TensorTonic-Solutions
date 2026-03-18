9940da6bdc4057baaa29df7e1adef5600207b777
Tuấn Võ <volantuan0908@gmail.com>
2026-03-18 08:26:51 +0700

    Add solution for sarsa-update

---
+++ sarsa-update/sarsa-update.py
@@ -0,0 +1,7 @@
+def sarsa_update(q_table, state, action, reward, next_state, next_action, alpha, gamma):
+    """
+    Perform one SARSA update and return the updated Q-table.
+    """
+    theta = reward + gamma * q_table[next_state][next_action] - q_table[state][action]
+    q_table[state][action] += alpha * theta
+    return q_table
